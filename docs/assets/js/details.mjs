353fff167629f3dbdef01b2154294fbced5d6279
Mads Stoumann Hansen <mads.stoumann@Stoumanns-MacBook-Pro.local>
2020-05-26 07:27:04 +0200

    Script update

--- docs/assets/js/details.mjs
+++ docs/assets/js/details.mjs
@@ -1,41 +1,28 @@
-/**
- * Details
- * @module details.mjs
- * @version 0.1.10
- * @summary 25-05-2020
- * @author Mads Stoumann
- * @description Helper-methods for animating `details`-tag
- */
-
  /**
- * @function animateDetails
+ * @function setDetailsHeight
  * @param {String} selector querySelector
- * @description Calculates height of <detail>-elements for smooth animations.
+ * @description Calculates height of <detail>-elements, both collapsed and expanded state
  */
-export function animateDetails(selector, wrapper = document) {
+export function setDetailsHeight(selector, wrapper = document) {
+	const setHeight = (detail, open = false) => {
+		detail.open = open;
+		const rect = detail.getBoundingClientRect();
+		detail.dataset.width = rect.width;
+		detail.style.setProperty(open ? `--expanded` : `--collapsed`,`${rect.height}px`);
+	}
 	const details = wrapper.querySelectorAll(selector);
 	const RO = new ResizeObserver(entries => {
 		return entries.forEach(entry => {
 			const detail = entry.target;
 			const width = parseInt(detail.dataset.width, 10);
 			if (width !== entry.contentRect.width) {
-				/* Clear previous calculations */
 				detail.removeAttribute('style');
-
-				detail.open = false;
-				let rect = detail.getBoundingClientRect();
-				detail.dataset.width = rect.width;
-				detail.style.setProperty(`--collapsed`,`${rect.height}px`);
-
-				/* Set [open] to enable getting dimensions of hidden panel */
-				detail.open = true;
-				rect = detail.getBoundingClientRect();
-				detail.style.setProperty(`--expanded`,`${rect.height}px`);
+				setHeight(detail);
+				setHeight(detail, true);
 				detail.open = false;
 			}
 		})
 	});
-
 	details.forEach(detail => {
 		RO.observe(detail);
 	});
